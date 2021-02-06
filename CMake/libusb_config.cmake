fe80ca24636385e9602a939d95d70ae21936ea3f
Silvio Traversaro <silvio.traversaro@iit.it>
2021-02-06 18:50:52 +0100

    Also search for libusb-1.0 in find_library

--- CMake/libusb_config.cmake
+++ CMake/libusb_config.cmake
@@ -1,5 +1,5 @@
 if (NOT TARGET usb)
-    find_library(LIBUSB_LIB usb-1.0)
+    find_library(LIBUSB_LIB usb-1.0 libusb-1.0)
     find_path(LIBUSB_INC libusb.h HINTS PATH_SUFFIXES libusb-1.0)
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(usb "libusb not found; using internal version" LIBUSB_LIB LIBUSB_INC)
